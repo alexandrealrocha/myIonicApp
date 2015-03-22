ce0954ff7443d7ff648e2afb0ef8e8a3f5f535fd
alexandrealrocha <alexandre.alrocha@gmail.com>
2015-03-22 19:58:18 -0300

    Nomes novos

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,34 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
+    name: 'David Lima',
     lastText: 'You on your way?',
     face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
   }, {
     id: 1,
-    name: 'Max Lynx',
+    name: 'Alessandro Oliveira',
     lastText: 'Hey, it\'s me',
     face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
   }, {
     id: 2,
-    name: 'Andrew Jostlin',
+    name: 'Gustavo Carvalho',
     lastText: 'Did you get the ice cream?',
     face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
   }, {
     id: 3,
-    name: 'Adam Bradleyson',
+    name: 'Rayner Lemos',
     lastText: 'I should buy a boat',
     face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
   }, {
     id: 4,
-    name: 'Perry Governor',
+    name: 'Leandro Ferreira',
     lastText: 'Look at my mukluks!',
     face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+  }, {
+    id: 5,
+    name: 'Raoni Soares',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
   }];
 
   return {
